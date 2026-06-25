6a52505a7cb9310fb15309207d44b434bd90e4d5
Gomolemo <masokogomolemo@gmail.com>
2026-06-25 11:53:34 +0200

    added new api key

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function search(event) {
   let searchInputElement = document.querySelector("#search-input");
   let city = searchInputElement.value;
 
-  let apiKey = "b2a5adcct04b33178913oc335f405433";
+  let apiKey = "12t3ead9cc49bf67a0ao50c2a2b4aa4f";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(displayTemperature);
